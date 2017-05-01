c573a1e83e33d2e535f7647f4118e14a976c5ae8
sorenwagemans <soren.wagemans@hotmail.com>
2017-05-02 00:23:06 +0200

    adding views

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -3,8 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Demo or Die' });
 });
-
+router.get('/choosegroups', function(req, res, next){
+    res.render("chooseGroups");
+});
+router.get('/profile', function(req, res, next){
+    res.render("profile");
+});
+router.get('/vote', function(req, res, next){
+    res.render("vote");
+});
+router.get('/adminview', function(req, res, next){
+    res.render("adminView");
+});
 
 module.exports = router;
