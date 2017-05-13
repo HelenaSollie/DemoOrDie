4d41d8a94b8638e12e59f85dc22acf12ccab84ce
sorenwagemans <soren.wagemans@hotmail.com>
2017-05-13 16:46:50 +0200

    readded index.js

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,6 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('home', { title: 'Demo or die' });
+});
+
+router.get('/login', function (req, res, next) {
+   res.render('login');
+});
+
+router.get('/home', function (req, res, next) {
+   res.render('home');
+});
+
+router.get('/profile', function (req, res, next) {
+   res.render('profile');
+});
+
+router.get('/chooseGroups', function (req, res, next) {
+    res.render('chooseGroups');
+});
+
+router.get('/adminView', function (req, res, next) {
+    res.render('adminView');
+});
+
+router.get('/adminlogin', function(req, res, next) {
+  res.render('adminlogin', { title: 'Demo or die | Admin Log In' });
+});
 
 
 module.exports = router;
