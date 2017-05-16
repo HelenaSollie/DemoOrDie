c0150396cba784bb49d3e88d436b837a5f3a3b46
sorenwagemans <soren.wagemans@hotmail.com>
2017-05-16 15:36:05 +0200

    trying to make login for admin

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -3,17 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 
-router.get('/home', function (req, res, next) {
-   res.render('home');
+router.get('/chooseGroups', require('connect-ensure-login').ensureLoggedIn(), function (req, res, next) {
+    res.render('chooseGroups',{ user: req.user });
 });
 
-router.get('/chooseGroups', function (req, res, next) {
-    res.render('chooseGroups');
+router.get('/adminView', require('connect-ensure-login').ensureLoggedIn(), function (req, res, next) {
+    router.post('/adminProfile',function(req,res){
+        var test = req.body.check;
+        if (test != "soren"){
+            console.log("yes");
+            }else{
+            console.log("no");
+        }
+
+    });
+    res.render('adminView',{ user: req.user });
+
 });
 
-router.get('/adminView', function (req, res, next) {
-    res.render('adminView');
-});
 
 
 module.exports = router;
