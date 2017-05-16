2505972717c3293bacbdf76989c281bca05fc955
sorenwagemans <soren.wagemans@hotmail.com>
2017-05-16 22:30:15 +0200

    fixed problems, back up to date

--- myapp/app.js
+++ myapp/app.js
@@ -1,25 +1,19 @@
 var express = require('express');
 var path = require('path');
-
 var favicon = require('serve-favicon');
-
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passport = require('passport');
 var fbAuth = require('./authentication.js');
-
 var User = require('./user.js');
 var Vakken = require('./vakken.js');
-var TeacherStudent = require('./teacherstudent.js');
-
+var teacherstudent = require('./teacherstudent.js');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1/demoordie');
-
 var config = require('./oauth.js');
+
 var index = require('./routes/index');
-
-
+mongoose.connect('mongodb://127.0.0.1/demoordie');
 
 
 passport.serializeUser(function(user, cb) {
@@ -34,35 +28,24 @@ passport.deserializeUser(function(obj, cb) {
 var app = express();
 
 // view engine setup
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 
 // Use application-level middleware for common functionality, including
-// logging, parsing, and session handling
-
+// logging, parsing, and session handling.
 app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 
 // Initialize Passport and restore authentication state, if any, from the
-// session
-
+// session.
 app.use(passport.initialize());
 app.use(passport.session());
 
 
-// Define routes
-app.get('/', function(req, res) {
-    res.render('home', { user: req.user });
-});
-
-app.get('/login', function(req, res) {
-    res.render('login', { title: 'Demo or die' });
-});
-
+// Define routes.
 app.get('/vote', require('connect-ensure-login').ensureLoggedIn(), function(req, res){
     res.render('vote', { user: req.user });
 });
@@ -74,6 +57,13 @@ app.get('/adminView', require('connect-ensure-login').ensureLoggedIn(), function
 app.get('/adminProfile', require('connect-ensure-login').ensureLoggedIn(), function(req, res){
     res.render('adminprofile', { user: req.user });
 });
+app.get('/', function(req, res) {
+    res.render('home', { user: req.user });
+});
+
+app.get('/login', function(req, res){
+    res.render('login', { title: 'Demo or die' });
+});
 
 app.get('/login/facebook', passport.authenticate('facebook'));
 
@@ -84,10 +74,8 @@ app.get('/login/facebook/return', passport.authenticate('facebook', { failureRed
 app.get('/profile', require('connect-ensure-login').ensureLoggedIn(), function(req, res){
     res.render('profile', { user: req.user });
 });
-
-
 app.post('/chooseGroups', function (req, res) {
-    var vakken = new Vakken({
+    vakken = new Vakken({
         php: req.body.php,
         webtech: req.body.webtech,
         project: req.body.project,
@@ -99,22 +87,21 @@ app.post('/chooseGroups', function (req, res) {
     });
 });
 
+
 app.post('/home', function(req, res) {
 
-    var TeacherStudent = new TeacherStudent({
+    teacherstudent = new TeacherStudent({
         student: req.body.student,
         teacher: req.body.teacher,
         userid: req.user.oauthID
     });
 
-    TeacherStudent.save(function (err, doc) {
+    teacherstudent.save(function (err, doc) {
         if (err) res.json(err);
         else    res.redirect('/profile');
     });
 
 });
-
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
