32f7653be107d3e01961f713dd190bb8232d6cb1
sorenwagemans <soren.wagemans@hotmail.com>
2017-05-16 11:12:36 +0200

    removed route home from index.js

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -2,20 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Demo or die' });
-});
-
-router.get('/login', function (req, res, next) {
-   res.render('login');
-});
 
 router.get('/home', function (req, res, next) {
    res.render('home');
-});
-
-router.get('/profile', function (req, res, next) {
-   res.render('profile');
 });
 
 router.get('/chooseGroups', function (req, res, next) {
